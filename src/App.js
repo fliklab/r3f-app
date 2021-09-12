6225fa91c5962314c3664b22cd9e87fdc1d4c636
Flik Jeong <jsh852@gmail.com>
2021-09-12 14:46:59 +0900

    useCannon이 적용된 큐브 example

--- src/App.js
+++ src/App.js
@@ -1,80 +1,42 @@
-import './App.css';
-// import Box from './components/Box';
-import { OrbitControls, Stats } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import { useMemo } from 'react';
+import { Physics, useBox, usePlane } from "@react-three/cannon";
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
+
+function Plane(props) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+  return (
+    <mesh ref={ref}>
+      <planeBufferGeometry args={[100, 100]} />
+    </mesh>
+  );
+}
+
+function Cube(props) {
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], ...props }));
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry />
+      <meshPhysicalMaterial/>
+    </mesh>
+  );
+}
 
 function App() {
-  const [positions, colors] = useMemo(() => {
-    let positions = [], colors = []
-  for (let i = 0; i < 4; i++) {
-    positions.push(1 - Math.random() * 2)
-    positions.push(1 - Math.random() * 2)
-    positions.push(1 - Math.random() * 2)
-    colors.push(0);
-    colors.push(0);
-    colors.push(0.5);
-  }
-  console.log('points', positions, colors);
-  return [new Float32Array(positions), new Float32Array(colors)]
-  }, [])
-
-
   return (
-    <>
-      <Canvas>
-        {/* <ambientLight />
-          <pointLight position={[0, 2, 0]} color='red' power={20}/>
-          <pointLight position={[0.5, 2, 0]} color='blue' power={20}/>
-          <pointLight position={[0, 3, 0]} color='pink' power={20}/>
-        <pointLight position={[10, 10, 10]} /> */}
-        <OrbitControls />
-        <line>
-          <bufferGeometry attach="geometry">
-            <bufferAttribute
-              attachObject={["attributes", "position"]}
-              count={positions.length / 3}
-              array={positions}
-              itemSize={3} />
-            <bufferAttribute
-              attachObject={["attributes", "color"]}
-              count={colors.length / 3}
-              array={colors}
-              itemSize={3} />
-          </bufferGeometry>
-          <lineBasicMaterial
-            attach="material"
-            sizeAttenuation={false}
-            lineWidth={50}
-            color={[1,0.5,0.5]}
-            linecap={'round'} linejoin={'round'}
-          />
-        </line>
-        <points>
-          <bufferGeometry attach="geometry">
-            <bufferAttribute
-              attachObject={["attributes", "position"]}
-              count={positions.length / 3}
-              array={positions}
-              itemSize={3} />
-            <bufferAttribute
-              attachObject={["attributes", "color"]}
-              count={colors.length / 3}
-              array={colors}
-              itemSize={3} />
-          </bufferGeometry>
-          <pointsMaterial
-            attach="material"
-            vertexColors
-            size={20}
-            sizeAttenuation={false}
-          />
-        </points>
-        <Stats />
-        <axesHelper />
-
-      </Canvas>
-    </>
+    <Canvas>
+      {/* <ambientLight /> */}
+      <Physics>
+        <pointLight position={[0, 2, 0]} color="red" power={20} />
+        <pointLight position={[0.5, 2, 0]} color="blue" power={20} />
+        <pointLight position={[0, 3, 0]} color="pink" power={20} />
+        <pointLight position={[10, 10, 10]} />
+        <Cube position={[0.2, 2, 0]} rotation={[0, 0, 0]} />
+        <Cube position={[0.1, 5, 0]} rotation={[0.11, 0.1, 0]} />
+        <Cube position={[-0.40, 7, 0]} rotation={[0.2, 0.1, 0]} />
+        <Cube position={[-0.1, 9, 0]} rotation={[0, 0.1, 0]} />
+        <Plane />
+      </Physics>
+    </Canvas>
   );
 }
 
