b667e893a87e5c27da542ba081d98ad38e6a859a
Flik Jeong <jsh852@gmail.com>
2021-09-29 00:58:00 +0900

    key Press Test

--- src/components/BoundingBoxPhysical.jsx
+++ src/components/BoundingBoxPhysical.jsx
@@ -1,6 +1,5 @@
 import { useBox } from '@react-three/cannon';
 
-
 const BoundingBoxPhysical = ({
     position = [0,0,0],
     offset = [0,0,0],
@@ -8,7 +7,29 @@ const BoundingBoxPhysical = ({
     visible = false,
     children,
 }) => {
-    const [ref, api] = useBox(() => ({ mass: 1, args: dims, position }));
+    const usedBox = useBox(() => ({ mass: 1, args: dims, position }));
+    const [ref, api] = usedBox;
+
+    document.addEventListener('keydown', function (event) {
+        switch (event.key) {
+            case 'w':
+                api.position.set(0, 1, 0);
+                console.log('usedBox',api)
+                break;
+          case 's':
+            api.position.set(1, 0, 0);
+            break;
+          case 'a':
+            api.position.set(0, 0, 1);
+            break;
+          case 'd':
+            api.position.set(-1, 0, 0);
+            break;
+          default:
+            break;
+        }
+        });
+
     return (
         <group ref={ref} api={api}>
             <mesh scale={dims} visible={visible}>
