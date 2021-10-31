ebd3433848818e3a03afbe2836d90d870660f0f3
Flik Jeong <jsh852@gmail.com>
2021-10-31 23:50:27 +0900

    add orbitcontrol to scene

--- src/Scenes/Scene1.js
+++ src/Scenes/Scene1.js
@@ -26,6 +26,7 @@ function Scene() {
 <pointLight position={[70, 100, 0]} color="blue" intensity={20} />
   <pointLight position={[-70, 80, -20]} color="pink" intensity={20} />
   {/* todo: use camera of recoil and update its target position, use lookat ? or worldDrirection ? and compute its direction */}
+    <OrbitControls ref={orbitRef} minDistance={1} maxDistance={20} target={[0,2.5,0]}/>
     <SkyBoxcene/>
     <Suspense fallback={null}>
       <Plane />
