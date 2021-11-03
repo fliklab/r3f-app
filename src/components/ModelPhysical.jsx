c66e4ee08d1d59c47ccac5a65995952e0a6c9353
Flik Jeong <jsh852@gmail.com>
2021-11-03 21:01:28 +0900

    update ModelPhysical

--- src/components/ModelPhysical.jsx
+++ src/components/ModelPhysical.jsx
@@ -1,73 +1,20 @@
 
-import { useFrame, useLoader } from '@react-three/fiber';
-import { useRef, useState } from 'react';
+import { useLoader } from '@react-three/fiber';
+import { useRef } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import BoundingBoxPhysical from './BoundingBoxPhysical';
 
 const Model = props => {
-  const [keyPressed, setKeyPressed] = useState({up:false, down:false, left:false, right:false})
     const modelRef = useRef();
-  document.addEventListener('keydown', function (event) {
-    console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`);
-    switch (event.key) {
-      case 'w':
-          setKeyPressed(keyPressed => {return {...keyPressed, up: true}});
-          break;
-      case 's':
-        setKeyPressed(keyPressed => { return { ...keyPressed, down: true } });
-        break;
-      case 'a':
-        setKeyPressed(keyPressed => { return { ...keyPressed, left: true } });
-        break;
-      case 'd':
-        setKeyPressed(keyPressed => { return { ...keyPressed, right: true } });
-        break;
-      default:
-        break;
-    }
-    });
-    document.addEventListener('keyup', function (event) {
-      console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed up`);
-      switch (event.key) {
-        case 'w':
-            setKeyPressed(keyPressed => {return {...keyPressed, up: false}});
-            break;
-        case 's':
-          setKeyPressed(keyPressed => { return { ...keyPressed, down: false } });
-          break;
-        case 'a':
-          setKeyPressed(keyPressed => { return { ...keyPressed, left: false } });
-          break;
-        case 'd':
-          setKeyPressed(keyPressed => { return { ...keyPressed, right: false } });
-          break;
-        default:
-          break;
-      }
-    });  
   
-    useFrame(() => {
-      if (modelRef.current != null ) {
-        if (keyPressed.left && !keyPressed.right)
-          modelRef.current.position.set(modelRef.current.position.x - 0.06, modelRef.current.position.y, modelRef.current.position.z);
-        else if(keyPressed.right)
-          modelRef.current.position.set(modelRef.current.position.x + 0.06, modelRef.current.position.y, modelRef.current.position.z);
-      }
-        if(keyPressed.up && !keyPressed.down)
-          modelRef.current.position.z -= 0.06;
-        else if(keyPressed.down)
-          modelRef.current.position.z += 0.06;
-    })
-  
-    //  const { path, ...props_ } = props;
     const model = useLoader(
             GLTFLoader,
-            process.env.PUBLIC_URL + "/assets/Dwarf Idle/Dwarf Idle.gltf"
+            process.env.PUBLIC_URL + props.path
         )
-    console.log(model);
+   
   return (
     <group ref={modelRef}>
-       <BoundingBoxPhysical visible position={[-1, 2, 0]} dims={[0.8, 4, 0.8]} rotation={[0.3, 0, 0]} >
+       <BoundingBoxPhysical visible position={[-1, 1, 0]}>
         <primitive object={model.scene} scale={[2,2,2]} position={[0,-2,0]} />;
        </BoundingBoxPhysical>
     </group>
