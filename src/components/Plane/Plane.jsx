54db7f82e31b1a8332b0cbcdb20a1c414c13b546
Flik Jeong <jsh852@gmail.com>
2021-11-14 03:01:07 +0900

    update new models in map3

--- src/components/Plane/Plane.jsx
+++ src/components/Plane/Plane.jsx
@@ -8,9 +8,11 @@ function Plane(props) {
 
     return (
       <mesh ref={ref} receiveShadow >
-        <PlaneModel path={PUBLIC_ENV_URL + "/assets/map2/BASKETBALL/BASKETBALL.gltf"} position={[15,-24,13]}/>
-        <PlaneModel path={PUBLIC_ENV_URL + "/assets/map2/BASE2/BASE.gltf"} position={[15,-24,13]}/>
-        <PlaneModel path={PUBLIC_ENV_URL + "/assets/map2/TEST/test.gltf"} position={[15,-24,13]}/>
+        <PlaneModel path={PUBLIC_ENV_URL + "/assets/map3/BASKETBALL02/BASKETBALL.gltf"} position={[15,-24,13]}/>
+        <PlaneModel path={PUBLIC_ENV_URL + "/assets/map3/BASE02/BASE.gltf"} position={[15,-24,13]}/>
+        <PlaneModel path={PUBLIC_ENV_URL + "/assets/map3/BASE04/BASE04.gltf"} position={[15,-24,13]}/>
+        <PlaneModel path={PUBLIC_ENV_URL + "/assets/map3/BASE0202/BASE02.gltf"} position={[15,-24,13]}/>
+        <PlaneModel path={PUBLIC_ENV_URL + "/assets/map3/BASE0302/BASE03.gltf"} position={[15,-24,13]}/>
         {/* <PlainAnimModel path="/assets/water/ocean_wave_test.gltf"/> */}
         {/* <planeBufferGeometry args={[100, 100]} /> */}
       </mesh>
