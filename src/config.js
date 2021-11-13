843028edd8a960b08fc450e7b9529a4feba88eab
Flik Jeong <jsh852@gmail.com>
2021-11-14 00:52:36 +0900

    update docs

--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
 export const PUBLIC_ENV_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://fliklab.github.io/r3f-app"
-    : "";
+  process.env.NODE_ENV === "production" ? "https://spacesummer.art/" : "";
